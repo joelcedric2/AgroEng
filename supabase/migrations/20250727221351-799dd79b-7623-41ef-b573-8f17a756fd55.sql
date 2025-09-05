b1371c03d79b4b02f1e1b44ca448106e073c1a76
joelcedric2 <77430619+joelcedric2@users.noreply.github.com>
2025-09-05 00:35:36 +0000

    Delete supabase directory

--- supabase/migrations/20250727221351-799dd79b-7623-41ef-b573-8f17a756fd55.sql
+++
@@ -1,6 +0,0 @@
--- Assign admin role to joelcedric237@gmail.com
-INSERT INTO public.user_roles (user_id, role)
-SELECT id, 'admin'::app_role
-FROM auth.users 
-WHERE email = 'joelcedric237@gmail.com'
-ON CONFLICT (user_id, role) DO NOTHING;
